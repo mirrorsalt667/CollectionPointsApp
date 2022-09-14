86ff35c9e5160bbcff4647c0cce2f911fb38ded1
mirrorsalt667 <79618127+mirrorsalt667@users.noreply.github.com>
2022-09-14 21:30:39 +0800

    Initial Commit

---
+++ CollectionPointsApp/CollectionPointsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionPointsAppApp.swift
+//  CollectionPointsApp
+//
+//  Created by 黃肇祺 on 2022/9/14.
+//
+
+import SwiftUI
+
+@main
+struct CollectionPointsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CollectionPointsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CollectionPointsApp
+//
+//  Created by 黃肇祺 on 2022/9/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
